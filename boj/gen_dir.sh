daa4d90fe9c00cb3dbfd5f468f9f6a9b4bf377e5
jungwookim <kjw0323@gmail.com>
2020-09-18 13:28:49 +0900

    fix gen file

--- boj/gen_dir.sh
+++ boj/gen_dir.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-mkdir $1
-touch ./$1/a.py
-touch ./$1/input.txt
+prob_num=$1
+
+if [ -z "$prob_num" ]
+  then
+    echo "Problem's number is needed"
+    exit 1
+fi
+
+if [ -d "$prob_num" ]
+  then
+    echo "already exist file"
+    exit
+fi
+
+mkdir $prob_num
+touch ./$prob_num/a.py, ./$prob_num/input.txt
+
+echo The No.$prob_num Problem is generated.
