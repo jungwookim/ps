d6fc88cc9eb0004e339391068811b0998dc2a257
jungwoo <50188177+jungwookim@users.noreply.github.com>
2021-07-24 15:25:12 +0900

    dp

---
+++ boj/16194/a.py
@@ -0,0 +1,19 @@
+input = __import__('sys').stdin.readline
+
+def solution():
+    n = int(input())
+    p = [0] + list(map(int, input().split()))
+
+    dp = [0 for _ in range(n+1)]
+
+    def f(x):
+        if x == 1:
+            dp[1] == p[1]
+
+        dp[x] = min([dp[x - i] + p[i] for i in range(1, x + 1)])
+
+    for i in range(1, n+1):
+        f(i)
+    return dp[n]
+
+print(solution())
